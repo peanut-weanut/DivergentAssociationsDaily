72e3c66874bf703a8d294fbe2b44c301d8375a27
Yllnor Rukovci <yllruk13@gmail.com>
2025-03-03 16:26:42 +0100

    added a new command to the makefile and made a special made file for  the render website

---
+++ ecs-server/divergent-association-task-main/rendertest.py
@@ -0,0 +1,46 @@
+
+from flask import Flask, request, jsonify, make_response
+import numpy as np #incase I figure out how to send shorts via json instead of the string im sending now
+
+#to run a debug version of this python backend, type
+#flask --app examples --debug run
+#in the terminal
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app, resources={
+    r"/*": {  # Match all routes
+        "origins": ["http://localhost:3000"],
+        "methods": ["GET", "POST", "OPTIONS"],
+        "allow_headers": ["Content-Type"],
+        "supports_credentials": False
+    }
+})
+@app.route('/', methods=['OPTIONS'])
+def handle_options():
+    response = make_response()
+    response.headers.add('Access-Control-Allow-Origin', 'http://localhost:3000')
+    response.headers.add('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+    return response
+
+@app.route('/', methods=['POST'])
+def handleListOfWords():
+    if request.method == 'OPTIONS':
+        response = make_response()
+        response.headers.add('Access-Control-Allow-Origin', 'http://localhost:3000')
+        response.headers.add('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+        return response
+    
+    print('test')
+    print('Received data: ', request.json)
+    print('test')
+    data = request.get_json()
+    print('test')
+    print('Received data: ', data)
+    #if 'data' in data and isinstance(data['data'], list):
+    response_data = {
+        'score': "12.34"
+    }
+    return jsonify(response_data), 200
