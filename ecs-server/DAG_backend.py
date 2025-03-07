58cbb8e19a4f3beba30c3e87db3ec378b4010a5c
Yllnor Rukovci <yllruk13@gmail.com>
2025-03-07 23:00:39 +0100

    added get function, added the csv file, made a python script taht adds words to the empty dates

--- ecs-server/DAG_backend.py
+++ ecs-server/DAG_backend.py
@@ -1,5 +1,7 @@
 import dat
 from flask import Flask, request, jsonify, make_response
+from csv import reader
+from datetime import date
 import numpy as np #incase I figure out how to send shorts via json instead of the string im sending now
 
 #to run a debug version of this python backend, type
@@ -9,9 +11,12 @@ from flask_cors import CORS
 
 # GloVe model from https://nlp.stanford.edu/projects/glove/
 #model = dat.Model("glove.840B.300d.txt", "words.txt")
-model = dat.Model("glove.6B.300d.txt", "words.txt")
+model = dat.Model("glove.840B.300d.txt", "words.txt")
 
 app = Flask(__name__)
+
+app.config['TOGGLE_TITLE'] = True
+
 CORS(app, resources={
     r"/*": {  # Match all routes
         "origins": ["http://localhost:3000"],
@@ -20,6 +25,7 @@ CORS(app, resources={
         "supports_credentials": False
     }
 })
+
 @app.route('/', methods=['OPTIONS'])
 def handle_options():
     response = make_response()
@@ -37,15 +43,30 @@ def handleListOfWords():
         response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
         return response
     
-    print('test')
-    print('Received data: ', request.json)
-    print('test')
+    #print('Received data: ', request.json)
     data = request.get_json()
-    print('test')
-    print('Received data: ', data)
+    #print('test')
+    #print('Received data: ', data)
     #if 'data' in data and isinstance(data['data'], list):
     score = model.dat(data)
     response_data = {
         'score': str(score)[:5]
     }
     return jsonify(response_data), 200
+
+@app.route('/', methods=['GET'])
+def returnThreeDailyWords():
+    app.config['TOGGLE_TITLE'] = not app.config['TOGGLE_TITLE']
+    todayswords = []
+    with open("Word Word Word List.csv", 'r', newline = '') as file:
+        datereader = reader(file)
+        for line in datereader:
+            if line[0] == date.today().strftime('%-m/%-d/%Y'):
+                todayswords.append(line[1])
+                todayswords.append(line[2])
+                todayswords.append(line[3])
+    response = {
+        "data" : todayswords,
+        "title" : app.config['TOGGLE_TITLE']
+    }
+    return response
