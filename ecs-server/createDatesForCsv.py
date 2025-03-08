75829cd52b4148c1e5837824d1efa72e510806d5
Yllnor Rukovci <yllruk13@gmail.com>
2025-03-08 04:30:17 +0100

    made functions that pick simple nouns for the csv

--- ecs-server/createDatesForCsv.py
+++ ecs-server/createDatesForCsv.py
@@ -2,24 +2,27 @@ import dat
 import csv
 import random
 
-def main():
-    print("model running")
-    model = dat.Model("glove.840B.300d.txt", "words.txt")
-    print("model done running")
-    with open("Word Word Word List.csv", 'r', newline = '') as wordslist:
-        reader = csv.reader(wordslist)
-        rows = list(reader)
-        for lines in rows:
-            if not lines[1]:
-                lines[1] = random.choice(list(model.returnWords()))
-                lines[2] = random.choice(list(model.returnWords()))
-                lines[3] = random.choice(list(model.returnWords()))
-    
-    print("done reading and setting up the rows, on to writing")
-    with open("Word Word Word List.csv", 'w', newline = '') as wordslist:
-        writer = csv.writer(wordslist)
-        for row in rows:
-            writer.writerow(row)
-    print("done writing, check the csv")
+with open("ourwords.txt", "r") as f:
+    nouns = [line.strip().lower() for line in f]
 
-main()
+print("model running")
+model = dat.Model("glove.840B.300d.txt", "words.txt")
+print("model done running")
+with open("Word Word Word List.csv", 'r', newline = '') as wordslist:
+    reader = csv.reader(wordslist)
+    rows = list(reader)
+    for lines in rows:
+        if not lines[1]:
+            x = 1
+            while x <= 3:
+                curattempt = random.choice(nouns)
+                if curattempt in model.returnWords():
+                    lines[x] = curattempt
+                    x += 1
+
+print("done reading and setting up the rows, on to writing")
+with open("Word Word Word List.csv", 'w', newline = '') as wordslist:
+    writer = csv.writer(wordslist)
+    for row in rows:
+        writer.writerow(row)
+print("done writing, check the csv")
