fb86c499f0a2f249f990d2fe2fe28b8eae9b8f25
Yllnor Rukovci <yllruk13@gmail.com>
2025-03-08 20:49:50 +0100

    made a better word picker for the csv

--- ecs-server/createNounsList.py
+++ ecs-server/createNounsList.py
@@ -4,6 +4,7 @@ with open("CSW.txt", "r") as f2:
 with open("ourwords.txt", "w") as f3:
     with open("nouns.txt", "r") as f1:
         for noun in f1:
-            noun = noun.strip().lower()
-            if noun in sw: 
-                f3.write(noun + "\n")
+            if len(noun) > 4 and len(noun) < 8:
+                noun = noun.strip().lower()
+                if noun in sw: 
+                    f3.write(noun + "\n")
