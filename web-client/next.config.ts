59879411e6b48ede6253ac495454f6caa538ee3d
peanut-weanut <109343736+peanut-weanut@users.noreply.github.com>
2025-03-10 10:16:12 -0700

    kill me

--- web-client/next.config.ts
+++ web-client/next.config.ts
@@ -1,8 +1,5 @@
 import type { NextConfig } from "next";
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
-module.exports = withBundleAnalyzer({});
+
 
 const nextConfig: NextConfig = {
   /* config options here */
