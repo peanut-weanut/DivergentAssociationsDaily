770ebb322e26c4003463ce0e94388568dbbc690d
peanut-weanut <109343736+peanut-weanut@users.noreply.github.com>
2025-03-18 11:54:22 -0700

    oops im gay

--- web-client/src/app/page.tsx
+++ web-client/src/app/page.tsx
@@ -6,12 +6,7 @@ export default function Home() {
   return (
     <main className="bg-white min-h-screen flex items-center justify-center py-6">
       {/* Mobile frame for small screens */}
-      <div className="md:hidden relative mx-auto my-4 w-[320px] h-[640px] bg-white rounded-[40px] shadow-xl overflow-hidden border-8 border-black">
-        {/* Phone speaker */}
-        <div className="absolute top-0 left-1/2 transform -translate-x-1/2 w-16 h-5 bg-white rounded-b-xl z-10 flex items-center justify-center">
-          <div className="w-2 h-2 rounded-full bg-gray-800"></div>
-        </div>
-        
+      <div className="md:hidden relative mx-auto my-4 bg-white">
         {/* App content */}
         <div className="bg-white h-full w-full overflow-y-auto">
           <MainLayout />
